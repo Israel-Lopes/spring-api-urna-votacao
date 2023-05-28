3fdf3a7259f3bdf12f98d842132a550281474b5e
Israel-Lopes <yisrael.lopes@gmail.com>
2023-05-27 21:27:02 -0300

    controller associado

--- src/main/java/com/urna/app/service/IAssociadoService.java
+++ src/main/java/com/urna/app/service/IAssociadoService.java
@@ -1,8 +1,36 @@
 package com.urna.app.service;
 
+import com.urna.app.service.interfaces.AssociadoInterface;
+import com.urna.app.service.model.Associado;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Service
-public class IAssociadoService {
+public class IAssociadoService implements AssociadoInterface {
 
+    @Override
+    public ResponseEntity getAssociado(HttpServletRequest request, Long id) {
+        return null;
+    }
+    @Override
+    public ResponseEntity<List<Associado>> getAssociadoList(HttpServletRequest request) {
+        return null;
+    }
+    @Override
+    public ResponseEntity createAssociado(@RequestBody Associado model) {
+        return null;
+    }
+    @Override
+    public ResponseEntity updateAssociado(Associado model) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity deleteAssociado(Long id) {
+        return null;
+    }
 }
